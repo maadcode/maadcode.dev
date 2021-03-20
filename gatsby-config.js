ead895dc9ed19c0a2c20e44a36531f36db6a0725
maadcode <manueldavila.developer@gmail.com>
2021-03-19 19:06:08 -0500

    Add new URL

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
     title: "Maadcode",
-    description: "Este es mi website personal donde muestro mis proyectos personales y artículos sobre programación.",
+    description: "Este es mi website personal donde muestro mis proyectos personales y artículos sobre desarrollo web.",
     author: "Manuel Dávila",
     twitterUsername: "maadcode",
     image: "/twitter-img.png",
-    siteUrl: "https://maadcode.netlify.app",
+    siteUrl: "https://maadcode.dev",
   },
   plugins: [
     `gatsby-transformer-sharp`,
